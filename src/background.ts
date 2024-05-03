10bf9cf07fd5cf95f940ed3e209ba1fdcc8d8d5b
iamrony777 <iamrony777@pm.me>
2024-05-03 23:07:50 +0530

    Add files

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,76 @@
-function polling() {
-  // console.log("polling");
-  setTimeout(polling, 1000 * 30);
-}
+// import { genComment } from "./langchain/instagram";
 
-polling();
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.action.setBadgeText({
+		text: "off",
+	});
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+	if (tab.status == "complete") {
+		const currentUrl = new URL(tab.url!);
+
+		if (currentUrl.host == "www.instagram.com") {
+			if (
+				currentUrl.pathname.startsWith("/p/") ||
+				currentUrl.pathname.startsWith("/reel/")
+			) {
+				console.log(`[${tabId!}] Current tab: ${currentUrl}`);
+
+				await chrome.action.setBadgeText({
+					tabId: tabId,
+					text: "on",
+				});
+
+				try {
+					await chrome.tabs.sendMessage(tabId!, {
+						type: "generate",
+						tab,
+					});
+				} catch (error) {
+					console.error(error);
+				}
+			}
+		} else {
+			await chrome.action.setBadgeText({
+				tabId,
+				text: "off",
+			});
+		}
+	}
+});
+
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+	// console.log(request, "recieved response in background ",);
+	const [tab] = await chrome.tabs.query({
+		active: true,
+		currentWindow: true,
+	});
+	if (request.type === "payload") {
+		const { description, comments, error } = request;
+		const response = await fetch(
+			"https://langchain.rony.in.eu.org/api/instagram/comment",
+			{
+				method: "POST",
+				body: JSON.stringify({ description, comments }),
+			}
+		);
+
+		const data = await response.json();
+		console.log({ type: "comment", data });
+		await chrome.tabs.sendMessage(tab.id!, {
+			type: "comment",
+			data,
+		}); // sendResponse({ type: 'comment', data});
+	}
+});
+
+// chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+// 	console.log(request, "recieved response in background ",);
+// });
+
+// chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {}
+
+// chrome.tabs.onUpdated.addListener(async (tabId, tab) => {
+
+// })
---
+++ src/content.ts
@@ -0,0 +1,69 @@
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+	if (request.type === "generate") {
+		newPost();
+	} else if (request.type === "comment") {
+		const { data } = request;
+		console.log({ comment: data.response }, "adding comment");
+		// addComment(data.response); // add comment
+	}
+	// sendResponse({ ...data, ...request });
+});
+
+// const addComment = (comment: string) => {
+// 	const textarea = document.querySelector('textarea[aria-label*="comment"]');
+// 	if (textarea) textarea.textContent = comment;
+// };
+const newPost = () => {
+	const genBtnExists = document.getElementById("gen-button") !== null;
+
+	console.log({ genBtnExists });
+
+	if (!genBtnExists) {
+		console.log("creating new button");
+		const genBtn = document.createElement("img");
+		genBtn.id = "gen-button";
+		genBtn.title = "click to generate";
+		genBtn.src = chrome.runtime.getURL("images/button/generate.png");
+		genBtn.height = 24;
+		genBtn.width = 24;
+		genBtn.addEventListener("click", async () => {
+			const description = document
+				.querySelector('meta[property="og:title"]')
+				?.getAttribute("content");
+
+			const comments: Array<string> = [];
+
+			const allComments = Array.from(
+				document.querySelectorAll(
+					'div[class="x78zum5 xdt5ytf x1iyjqo2"] > div span[dir][style]:not(:has(> *))'
+				)
+			).filter((el) => !el.textContent!.startsWith("View all"));
+
+			// const allAuthors = document.querySelectorAll(
+			// 	'div[class="x78zum5 xdt5ytf x1iyjqo2"] > div span[dir]:not([style]):not(:has(> *))'
+			// );
+
+			for (let i = 0; i < allComments.length; i++) {
+				comments.push(allComments[i]?.textContent || "");
+			}
+
+			// console.log({ description, comments, error: undefined });
+			await chrome.runtime.sendMessage({
+				type: "payload",
+				description,
+				comments,
+				error: undefined,
+			});
+		});
+
+		const textarea = document.querySelector('textarea[aria-label*="comment"]');
+		if (textarea) {
+			const parentCommentContainer = textarea.parentElement;
+			if (parentCommentContainer) {
+				parentCommentContainer.appendChild(genBtn);
+			}
+		}
+	}
+
+	// console.log({ description: "", comments: [], error: "Something went wrong" });
+};
